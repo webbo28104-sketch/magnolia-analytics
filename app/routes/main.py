9fae0d72ec4f792ff48216480811281e1ee13ac6
webbo28104-sketch <webbo28104@gmail.com>
2026-03-17 11:25:01 +0000

    Add /glossary route to main blueprint

--- app/routes/main.py
+++ app/routes/main.py
@@ -3,12 +3,14 @@ from flask_login import current_user
 
 main_bp = Blueprint('main', __name__)
 
-
 @main_bp.route('/')
 def index():
     return render_template('index.html')
 
-
 @main_bp.route('/pricing')
 def pricing():
     return render_template('pricing.html')
+
+@main_bp.route('/glossary')
+def glossary():
+    return render_template('glossary.html')
